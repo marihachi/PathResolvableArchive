9ce60fb5ff1d3528c3a2c361c1f50bd7550bcb57
marihachi <marihachi0620@gmail.com>
2019-10-06 19:33:48 +0900

    implement the ReadArchive method

--- src/ArchiveStreamManager.cs
+++ src/ArchiveStreamManager.cs
@@ -17,45 +17,69 @@ namespace PathResolvableArchive
 
 			stream.Position = 0;
 
-			var writer = new BinaryWriter(stream);
-
-			// write magic-number (3 bytes)
-			writer.Write(Encoding.ASCII.GetBytes("PRA"));
-
-			// write length of the json metadata (4 bytes)
-			writer.Write(metadataJson.Length);
-
-			// write the json metadata
-			writer.Write(Encoding.UTF8.GetBytes(metadataJson));
-
-			// file data
-			foreach (var file in files)
+			using(var writer = new BinaryWriter(stream, Encoding.UTF8, true))
 			{
-				writer.Write(file.Buffer.ToArray());
+				// write magic-number (3 bytes)
+				writer.Write(Encoding.ASCII.GetBytes("PRA"));
+
+				// write length of the json metadata (4 bytes)
+				writer.Write(metadataJson.Length);
+
+				// write the json metadata
+				writer.Write(Encoding.UTF8.GetBytes(metadataJson));
+
+				// file data
+				foreach (var file in files)
+				{
+					writer.Write(file.Buffer.ToArray());
+				}
 			}
 		}
 
-		public static ArchiveMetaData ReadHeader(Stream stream)
+		public static List<BufferWithPathInfo> ReadArchive(Stream stream)
 		{
+			var result = new List<BufferWithPathInfo>();
+
 			stream.Position = 0;
 
-			var reader = new BinaryReader(stream);
-
-			// read magic-number (3 bytes)
-			if (Encoding.ASCII.GetString(reader.ReadBytes(3)) != "PRA")
+			using(var reader = new BinaryReader(stream, Encoding.UTF8, true))
 			{
-				throw new FormatException("invalid PRA header");
+				/* reading header */
+
+				// read magic-number (3 bytes)
+				if (Encoding.ASCII.GetString(reader.ReadBytes(3)) != "PRA")
+				{
+					throw new FormatException("invalid PRA header");
+				}
+
+				// read length of the json metadata (4 bytes)
+				var metadataLnegth = reader.ReadInt32();
+
+				// read the json metadata
+				var metadataBuffer = reader.ReadBytes(metadataLnegth);
+				var metadataJson = Encoding.UTF8.GetString(metadataBuffer);
+				var metadata = ArchiveMetaData.FromJson(metadataJson);
+
+				/* reading payload */
+
+				var baseOffset = (int)stream.Position;
+
+				if (metadata.Version == 1)
+				{
+					foreach (var file in metadata.Files)
+					{
+						stream.Position = baseOffset + int.Parse(file.Offset);
+						var buffer = reader.ReadBytes(int.Parse(file.Length));
+						result.Add(new BufferWithPathInfo { Buffer = buffer, Path = file.Path });
+					}
+				}
+				else
+				{
+					throw new FormatException("format version is not supported");
+				}
 			}
 
-			// read length of the json metadata (4 bytes)
-			var metadataLnegth = reader.ReadInt32();
-
-			// read the json metadata
-			var metadataBuffer = reader.ReadBytes(metadataLnegth);
-			var metadataJson = Encoding.UTF8.GetString(metadataBuffer);
-			var metadata = ArchiveMetaData.FromJson(metadataJson);
-
-			return metadata;
+			return result;
 		}
 	}
 }
